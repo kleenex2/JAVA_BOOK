31bc46d6e26afdce9db9ace8d1419ff1880464bf
nadat <nadat@192.168.219.102>
2023-01-05 17:50:42 +0900

    자바1

---
+++ src/Ch04/C01Scanner.java
@@ -0,0 +1,10 @@
+package Ch04;
+
+public class C01Scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Ch05/C04Shift연산자.java
@@ -0,0 +1,16 @@
+package Ch05;
+
+public class C04Shift연산자 {
+
+	public static void main(String[] args) {
+		int num1 = 15;    // 00000000 00000000 00000000 00001111
+		int num2 = 20;    // 00000000 00000000 00000000 00010100
+		int num3 = num2<<2;
+		int num4 = num2>>2;
+		
+		System.out.println("<<Shift 연산 결과 : " + num3);
+		System.out.println(">>Shift 연산 결과 : " + num4);
+
+	}
+
+}
